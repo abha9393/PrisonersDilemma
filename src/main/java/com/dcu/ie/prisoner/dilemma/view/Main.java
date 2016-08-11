07fdb3fdc53edc5b63346b6120e126cf7dbf7367
abha9393 <abha.aggarwal2@mail.dcu.ie>
2016-08-11 18:18:45 +0100

    adding changing scenes

--- src/main/java/com/dcu/ie/prisoner/dilemma/view/Main.java
+++ src/main/java/com/dcu/ie/prisoner/dilemma/view/Main.java
@@ -1,8 +1,7 @@
 package com.dcu.ie.prisoner.dilemma.view;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,13 +9,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("parentScreen.fxml"));
+        SceneManager sceneManager = new SceneManager();
+
+        sceneManager.loadScene(SceneName.SetNumberOfPlayers.toString());
+        sceneManager.loadScene(SceneName.LaunchGame.name());
+
+        sceneManager.setScene(SceneName.SetNumberOfPlayers);
+
+        Group root = new Group();
+        root.getChildren().addAll(sceneManager.getSceneLayout());
+
         primaryStage.setTitle("Prisoner's Dilemma");
         primaryStage.setScene(new Scene(root, 300, 275));
 
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/main/java/com/dcu/ie/prisoner/dilemma/view/SceneName.java
@@ -0,0 +1,10 @@
+package com.dcu.ie.prisoner.dilemma.view;
+
+/**
+ * @author Abha Aggarwal
+ * @version 1.0
+ * @since 09-08-2016.
+ */
+public enum SceneName {
+    SetNumberOfPlayers, LaunchGame;
+}
